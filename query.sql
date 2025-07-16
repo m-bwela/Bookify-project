82e28494c28abea346307bd5ca8046b562c75d11
TyeMuzan <TyeMuzan@gmail.com>
2025-07-16 13:06:49 +0300

    query to fetch user data

--- query.sql
+++ query.sql
@@ -1,13 +1,14 @@
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
-    title VARCHAR(255) UNIQUE NOT NULL,
-    author VARCHAR(250),
-    cover_id INTEGER
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(250) NOT NULL,
+    cover_id INTEGER,
+    UNIQUE (title, author)
 );
 
 CREATE TABLE book_reviews (
     review_id SERIAL PRIMARY KEY,
-    book_id INTEGER REFERENCES books(book_id),
+    book_id INTEGER NOT NULL REFERENCES books(book_id) ON DELETE CASCADE,
     rating INTEGER CHECK (rating >= 1 AND rating <= 5),
     review_text TEXT,
     review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
